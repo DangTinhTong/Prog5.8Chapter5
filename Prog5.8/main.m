6bd568218f75a0f025890a5937e4f1354d0fbd43
admin <dangtinhtong@gmail.com>
2017-11-24 23:48:37 +0700

    First commit

--- Prog5.8/main.m
+++ Prog5.8/main.m
@@ -5,13 +5,23 @@
 //  Created by admin on 11/24/17.
 //  Copyright © 2017 admin. All rights reserved.
 //
-
+// Program to reverse the digits of a number
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+       // NSLog(@"Hello, World!");
+        
+        
+        int number, right_digit;
+        NSLog(@"Enter your number.");
+        scanf("%i", &number);
+        while (number!=0) {
+            right_digit=number%10;
+            NSLog(@"%i", right_digit) ;
+            number/=10;
+        }
     }
     return 0;
 }
